60be5606651853dfc695e404eabfb331e8d78c99
bipinparajuli <bipinprjl@gmail.com>
2021-04-02 10:52:09 -0700

    simple animation

--- src/Component/UI/Card/Card.js
+++ src/Component/UI/Card/Card.js
@@ -5,11 +5,11 @@ import {FaUser} from 'react-icons/fa'
 export const Card = ({totalbook,title}) => {
     return (
         <div>
-            <div class="card" style={{width: "18rem"}}>
-  <div class="card-body">
-    <h5 class="card-title"><FaUser /></h5>
-    <h6 class="card-subtitle mb-2">{title}</h6>
-    <p class="card-text">{totalbook}</p>
+            <div className="card animate__animated  animate__fadeInTopLeft" style={{width: "18rem"}}>
+  <div className="card-body" className="animate__animated  animate__fadeInTopLeft">
+    <h5 className="card-title"><FaUser /></h5>
+    <h6 className="card-subtitle mb-2">{title}</h6>
+    <p className="card-text">{totalbook}</p>
   </div>
 </div>
         </div>
